a467b2e8bceb66691c1b33dcd8c61975cc01da87
0-BSCode <bryanipsanchez@gmail.com>
2024-04-21 21:02:14 +0800

    Configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ["plugin:react-hooks/recommended", "universe", "prettier"],
+  rules: {
+    // Ensures props and state inside functions are always up-to-date
+    "react-hooks/exhaustive-deps": "warn"
+  }
+};
