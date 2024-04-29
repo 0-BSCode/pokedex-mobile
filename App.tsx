5adfa00eb8642b45f2e38d345bafb27190957c9b
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-04-30 00:16:12 +0800

    Install and configure nativewind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! haha</Text>
+    <SafeAreaView className="h-screen">
       <StatusBar style="auto" />
-    </View>
+      <View className="bg-blue-500 h-screen ">
+        <Text className="text-2xl font-black mx-5 text-center">
+          Open up App.tsx to start working on your app!
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#aaa",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#aaa",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
