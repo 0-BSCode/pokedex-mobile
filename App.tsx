4ec913a69dc6ce2fe28ed6bc5e5084d0619a9a49
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-04-30 09:52:36 +0800

    Remove stylesheet

--- App.tsx
+++ App.tsx
@@ -1,24 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView className="h-screen">
-      <StatusBar style="auto" />
-      <View className="bg-blue-500 h-screen ">
-        <Text className="text-2xl font-black mx-5 text-center">
-          Open up App.tsx to start working on your app!
-        </Text>
-      </View>
+    <SafeAreaView className="flex justify-center h-screen bg-blue-500">
+      <Text className="mx-5 text-2xl font-black text-center">
+        Open up App.tsx to start working on your app!
+      </Text>
     </SafeAreaView>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#aaa",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   }
-// });
