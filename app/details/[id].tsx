2f9669eb7c8ee6d75f813ef1e319e152a2169724
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-05-06 12:20:46 +0800

    Create convertToTitleCase  util

--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -1,6 +1,7 @@
 import { useLocalSearchParams } from "expo-router";
 import { View, Image, Text, StyleSheet } from "react-native";
 
+import convertToTitleCase from "../../src/_utils/convertToTitleCase";
 import usePokemonStore from "../../src/stores/pokemonStore";
 
 export default function DetailsPage() {
@@ -10,45 +11,42 @@ export default function DetailsPage() {
     const viewedPokemon = pokemonList[Number(id) - 1];
 
     return (
-        <View className="flex-1 px-4 py-8 bg-white">
+        <View className="flex-1 px-5 py-8 bg-white">
             <View className="flex flex-row items-center justify-between mb-8">
-                <Text className="text-2xl font-bold">{viewedPokemon.name}</Text>
+                <Text className="text-2xl font-bold">
+                    {convertToTitleCase(viewedPokemon.name)}
+                </Text>
                 <Text className="text-lg text-gray-500">#001</Text>
             </View>
-            <View className="flex flex-row">
-                <Image
-                    src={viewedPokemon.photoUrl}
-                    className="w-24 h-24 mr-4"
-                />
-                <View className="flex-1">
-                    <Text className="mb-2 text-base">Species: Seed</Text>
-                    <Text className="mb-2 text-base">
-                        Height: 23.6" (0.70 cm)
-                    </Text>
-                    <Text className="mb-2 text-base">
-                        Weight: 15.2 lbs (6.9 kg)
-                    </Text>
-                    <Text className="mb-2 text-base">
-                        Abilities: Overgrow, Chlorophyll
-                    </Text>
-                    <Text className="mb-2 text-base">
-                        Breeding: Gender Ratio 87.5% ♂ - 12.5% ♀
-                    </Text>
-                    <Text className="mb-2 text-base">Egg Groups: Monster</Text>
-                    <Text className="mb-2 text-base">Egg Cycle: Grass</Text>
-                </View>
-            </View>
-            <View className="flex flex-row justify-between mt-8">
-                <Text className="px-4 py-2 text-base bg-gray-200 rounded-md">
+            <View className="flex flex-row gap-2 ">
+                <Text className="px-6 py-2 text-base bg-gray-200 rounded-3xl">
                     Grass
                 </Text>
-                <Text className="px-4 py-2 text-base bg-purple-200 rounded-md">
+                <Text className="px-6 py-2 text-base bg-gray-200 opacity-25 rounded-3xl">
                     Poison
                 </Text>
             </View>
+            <View className="flex flex-row justify-center">
+                <Image
+                    src={viewedPokemon.photoUrl}
+                    className="mr-4 w-36 h-36"
+                />
+            </View>
+
             <View className="mt-8">
-                <Text className="mb-4 text-lg font-bold">Base Stats</Text>
-                {/* Add stat blocks here */}
+                <Text className="mb-2 text-base">Species: Seed</Text>
+                <Text className="mb-2 text-base">Height: 23.6" (0.70 cm)</Text>
+                <Text className="mb-2 text-base">
+                    Weight: 15.2 lbs (6.9 kg)
+                </Text>
+                <Text className="mb-2 text-base">
+                    Abilities: Overgrow, Chlorophyll
+                </Text>
+                <Text className="mb-2 text-base">
+                    Breeding: Gender Ratio 87.5% ♂ - 12.5% ♀
+                </Text>
+                <Text className="mb-2 text-base">Egg Groups: Monster</Text>
+                <Text className="mb-2 text-base">Egg Cycle: Grass</Text>
             </View>
         </View>
     );
