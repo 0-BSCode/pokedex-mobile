e7712869e6856dc7723509ed3d5d0977145900ba
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-05-07 09:33:24 +0800

    Finalize format

--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -17,17 +17,17 @@ export default function DetailsPage() {
                     <Text className="text-3xl font-black text-white">
                         {convertToTitleCase(viewedPokemon.name)}
                     </Text>
-                    <Text className="text-lg text-gray-500">#001</Text>
+                    <Text className="text-lg font-bold text-white">#001</Text>
                 </View>
-                <View className="flex flex-row gap-2 ">
-                    <View className="bg-white/50 rounded-3xl">
-                        <Text className="px-6 py-2 font-bold text-white ">
+                <View className="flex flex-row gap-2 py-0">
+                    <View className="flex items-center justify-center bg-white/50 rounded-3xl">
+                        <Text className="px-5 py-1 font-bold text-white ">
                             Grass
                         </Text>
                     </View>
 
                     <View className="bg-white/50 rounded-3xl">
-                        <Text className="px-6 py-2 font-bold text-white ">
+                        <Text className="px-5 py-1 font-bold text-white ">
                             Poison
                         </Text>
                     </View>
@@ -40,20 +40,42 @@ export default function DetailsPage() {
                 </View>
             </View>
 
-            <View className="h-full px-5 pt-8 mt-8 bg-white rounded-3xl">
-                <Text className="mb-2 text-base">Species: Seed</Text>
-                <Text className="mb-2 text-base">Height: 23.6" (0.70 cm)</Text>
-                <Text className="mb-2 text-base">
-                    Weight: 15.2 lbs (6.9 kg)
-                </Text>
-                <Text className="mb-2 text-base">
-                    Abilities: Overgrow, Chlorophyll
-                </Text>
-                <Text className="mb-2 text-base">
-                    Breeding: Gender Ratio 87.5% ♂ - 12.5% ♀
-                </Text>
-                <Text className="mb-2 text-base">Egg Groups: Monster</Text>
-                <Text className="mb-2 text-base">Egg Cycle: Grass</Text>
+            <View className="h-full px-5 pt-5 mt-8 bg-white rounded-3xl">
+                <View className="flex flex-row justify-between py-4 text-md">
+                    <Text>About</Text>
+                    <Text>Base Stats</Text>
+                    <Text>Evolution</Text>
+                    <Text>Moves</Text>
+                </View>
+                <View className="flex flex-row gap-10">
+                    <View className="flex ">
+                        <Text className="mb-2 text-base">Species</Text>
+                        <Text className="mb-2 text-base">Height</Text>
+                        <Text className="mb-2 text-base">Weight</Text>
+                        <Text className="mb-2 text-base">Abilities</Text>
+                    </View>
+                    <View className="flex ">
+                        <Text className="mb-2 text-base">Seed</Text>
+                        <Text className="mb-2 text-base">Height</Text>
+                        <Text className="mb-2 text-base">Weight</Text>
+                        <Text className="mb-2 text-base">Abilities</Text>
+                    </View>
+                </View>
+                <View className="flex">
+                    <Text className="text-lg font-bold">Breeding</Text>
+                    <View className="flex flex-row gap-10">
+                        <View className="flex">
+                            <Text className="mb-2 text-base">Gender</Text>
+                            <Text className="mb-2 text-base">Egg Groups</Text>
+                            <Text className="mb-2 text-base">Egg Cycle</Text>
+                        </View>
+                        <View className="flex ">
+                            <Text className="mb-2 text-base">Egg Groups</Text>
+                            <Text className="mb-2 text-base">Egg Groups</Text>
+                            <Text className="mb-2 text-base">Egg Cycle</Text>
+                        </View>
+                    </View>
+                </View>
             </View>
         </View>
     );
