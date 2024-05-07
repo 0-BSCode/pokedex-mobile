6654cf6b1694fd50f3d227f7e451d98c302ae6c1
0-BSCode <bryanipsanchez@gmail.com>
2024-05-07 09:34:06 +0800

    Fix expo router config for mobile

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
+process.env.EXPO_ROUTER_APP_ROOT = '../../app';
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"]
+    plugins: [
+      "nativewind/babel",
+      ['module:react-native-dotenv'],
+    ],
+    
   };
 };
