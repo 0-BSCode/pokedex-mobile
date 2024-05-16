ee51db170a2a562b99d14513ca0bf827ee989fec
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-05-16 19:17:15 +0800

    remove code in home page

--- app/home.tsx
+++ app/home.tsx
@@ -4,9 +4,6 @@ import { Text, View, Button } from "react-native";
 export default function Home() {
     return (
         <View>
-            <Text className="mx-5 text-2xl font-black text-center ">
-                Eyyy we at the home page cuhh
-            </Text>
             <Button
                 onPress={() => {
                     router.navigate("/");
